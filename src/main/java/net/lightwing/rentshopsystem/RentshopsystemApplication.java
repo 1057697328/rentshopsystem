fe0c1726a641193abb27a3bb4ce6b8e82c1b25fd
Clarence <1057697328@qq.com>
2018-11-12 22:57:20 +0800

    项目初始化,第1次上传,整个项目架构

---
+++ src/main/java/net/lightwing/rentshopsystem/RentshopsystemApplication.java
@@ -0,0 +1,12 @@
+package net.lightwing.rentshopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentshopsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentshopsystemApplication.class, args);
+    }
+}
---
+++ src/test/java/net/lightwing/rentshopsystem/RentshopsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package net.lightwing.rentshopsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentshopsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
